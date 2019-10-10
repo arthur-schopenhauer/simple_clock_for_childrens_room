02b71076174ebaa60fb7ea883d385e0f17e20257
Gabriel W Irizarry Olivera <gabriel161616@gmail.com>
2019-10-09 17:33:41 -0700

    Update date_and_time.js

--- date_and_time.js
+++ date_and_time.js
@@ -1,1 +1,50 @@
-body = document.getElementById("body");
+//partOfDay() returns string timeOfDay
+var partOfDay = function() {
+    var time = new Date().getHours();
+    var minutes = new Date().getMinutes();
+    var timeOfDay = (
+        time > 5 && time < 12 ? "morning" :
+        time == 12 && min == 0 ? "noon" :
+        time >= 12 && time < 17 ? "afternoon" :
+        time >= 17 && time < 20 ? "evening" :
+        time >= 20 && time < 23 ? "night" :
+        time == 23 && minutes >= 59 ? "night" :
+        time == 0 && minutes == 0 ? "midnight" :
+        time >= 0 && time <= 5 ? "before dawn" :
+        "check timeOfDay variable"
+    );
+    return timeOfDay
+};
+
+
+var information = function() {
+    document.getElementById("weekday").innerText = moment().format('dddd');
+    document.getElementById("timeOfDay").innerText = partOfDay();
+    document.getElementById("hourMinuteSecond").innerText = moment().format('LT');
+    document.getElementById("dateOfTheMonth").innerText = moment().format('LL');
+};
+
+
+window.setInterval(information, 1000);
+
+// cancelFullScreen (and similarly webkitCancelFullScreen needs to be called on document, whereas requestFullscreen needs to be called on document.documentElement.
+function toggleFullScreen() {
+    var doc = document.documentElement,
+        state = (document.webkitIsFullScreen || document.isFullScreen),
+        requestFunc = (doc.requestFullscreen || doc.webkitRequestFullScreen),
+        cancelFunc = (document.cancelFullScreen || document.webkitCancelFullScreen);
+
+    (!state) ? requestFunc.call(doc): cancelFunc.call(document);
+};
+
+
+function togglePause() {
+    var audio = document.querySelector('audio');
+     if (audio.paused && !audio.ended) {
+        audio.muted = false;
+        audio.play();
+     } else {
+        audio.muted = true;
+        audio.pause();
+     }
+};
